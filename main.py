66f14e77ad794b714ae8f7b28878528f905b7291
Adam Hill <adam@diginc.us>
2022-11-30 23:34:35 -0600

    Day 1 one func two parts

--- main.py
+++ main.py
@@ -8,34 +8,19 @@ def read_input(file):
 def day1a(input):
     elves = input.strip().split('\n\n')
     elves_inventories = [e.strip().split('\n') for e in elves]
-    most_cals = 0
-    for elf in elves_inventories:
-        print(f"What ya got {elf}")
-        cals = sum([int(c) for c in elf])
-        if cals > most_cals:
-            print(f"New most: {most_cals} {elf}")
-            most_cals = cals
-        else:
-            print("Not most")
-
-    print(f"Most calories an elf has: {most_cals}")
-
-
-def day1b(input):
-    elves = input.strip().split('\n\n')
-    elves_inventories = [e.strip().split('\n') for e in elves]
     all_cals = []
     for elf in elves_inventories:
         cals = sum([int(c) for c in elf])
         all_cals.append(cals)
 
+    print(f"Day 1 Part 1, most cal elf: {all_cals[-1]}")
     top_3_cals = sorted(all_cals)[-3:]
-    print(f"Most calories an elf has x3: {top_3_cals} = {sum(top_3_cals)}")
+    print(f"Day 1 Part 2, Most calories an elf has x3: {top_3_cals} = {sum(top_3_cals)}")
 
 
 if __name__ == '__main__':
-    day_filter = "1"
-    part_filter = "b"
+    day_filter = "[1-25]"
+    part_filter = "[ab]"
     p = pathlib.Path()
     for input in p.glob(f"input/day{day_filter}{part_filter}.txt"):
         func = input.name.replace(".txt", "")
