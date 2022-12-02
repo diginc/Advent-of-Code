5c1c56c517d03494ec7ff971d9eeacb25118be6b
Adam Hill <adam@diginc.us>
2022-12-01 23:48:41 -0600

    Cleanup Day 2

--- aoc2022/day2.py
+++ aoc2022/day2.py
@@ -9,32 +9,37 @@ p1_scores = {
   "Y": 2,
   "Z": 3
 }
+rock = "Rock"
+paper = "Paper"
+scissors = "Scissors"
+map = {
+    'A': rock,
+    "B": paper,
+    "C": scissors,
+    "X": rock,
+    "Y": paper,
+    "Z": scissors
+}
 
 x_map = {  # LOSE
-    'rock': 3,
-    'paper': 1,
-    'scissors': 2,
+    'A': 3,
+    'B': 1,
+    'C': 2,
 }
 y_map = {  # DRAW
-    'rock': 1,
-    'paper': 2,
-    'scissors': 3,
+    'A': 1,
+    'B': 2,
+    'C': 3,
 }
 z_map = {  # WIN
-    'rock': 2,
-    'paper': 3,
-    'scissors': 1,
+    'A': 2,
+    'B': 3,
+    'C': 1,
 }
 
 
 
 def day2(input, title):
-    input = input.replace('A', "rock")
-    input = input.replace('B', "paper")
-    input = input.replace('C', "scissors")
-    # input = input.replace('X', "rock")
-    # input = input.replace('Y', "paper")
-    # input = input.replace('Z', "scissors")
     input_list = input.split('\n')
 
     p1_answer = 0
@@ -42,27 +47,26 @@ def day2(input, title):
 
     for line in input_list:
         win = False
-        lose = False
         draw = False
         enemy, you = line.split(' ')
-        if you == enemy:
+        trnsl_enemy = map[enemy]
+        trnsl_you = map[you]
+        if trnsl_you == trnsl_enemy:
             draw = True
-        if you == "X":  # Rock
-            if enemy == "scissors":
+        if you == "X":  # Rock / Lose
+            if trnsl_enemy == scissors:
                 win = True
             p2_answer += x_map[enemy]
-        if you == "Y":  # Paper
-            if enemy == "rock":
+        if you == "Y":  # Paper / Draw
+            if trnsl_enemy == rock:
                 win = True
             p2_answer += 3
             p2_answer += y_map[enemy]
-        if you == "Z":  # Scissors
-            if enemy == "paper":
+        if you == "Z":  # Scissors / Win
+            if trnsl_enemy == paper:
                 win = True
             p2_answer += 6
             p2_answer += z_map[enemy]
-        if not win and not draw:
-            lose = True
 
         p1_answer += p1_scores[you]
         if win:
