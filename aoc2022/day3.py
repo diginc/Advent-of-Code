e4e45139ac55e33f691af0faa63c32497a7c3eea
Adam Hill <adam@diginc.us>
2022-12-02 23:51:06 -0600

    2022 day3 p2

--- aoc2022/day3.py
+++ aoc2022/day3.py
@@ -14,8 +14,10 @@ def main(input):
     p2_answer = 0
 
     dupes = []
+    badges = []
+    buffer = []
     for line in input:
-
+        # Part 1
         size = len(line)
         half = (size/2)
         c1 = [val for i, val in enumerate(line) if i < half]
@@ -24,21 +26,23 @@ def main(input):
             if char in c2:
                 dupes.append(char)
                 break
+        # Part 2
+        buffer.append(line)
+        if len(buffer) == 3:
+            for char in buffer[0]:
+                if char in buffer[1] and char in buffer[2]:
+                    badges.append(char)
+                    buffer = []
+                    break
 
-    # Priority math
-    assert ord('a') - 96 == 1
-    assert ord('z') - 96 == 26
-    assert ord('A') - 64 + 26 == 27
-    assert ord('Z') - 64 + 26 == 52
 
     for d in dupes:
-        offset = 0
-        if re.match('[a-z]', d):
-            offset = -96
-        if re.match('[A-Z]', d):
-            offset = -38
-        score = ord(d) + offset
+        score = get_score(d)
         p1_answer += score
+
+    for d in badges:
+        score = get_score(d)
+        p2_answer += score
 
 
 
@@ -47,5 +51,21 @@ def main(input):
     print(f"Day {DAY} Part 2: {p2_answer}")
 
 
+def get_score(d):
+    offset = 0
+    if re.match('[a-z]', d):
+        offset = -96
+    if re.match('[A-Z]', d):
+        offset = -38
+    score = ord(d) + offset
+    return score
+
+
 if __name__ == '__main__':
+    # Priority math
+    assert ord('a') - 96 == 1
+    assert ord('z') - 96 == 26
+    assert ord('A') - 64 + 26 == 27
+    assert ord('Z') - 64 + 26 == 52
+
     main(read_input(f"day{DAY}.txt", True))
