5c799ffa76031226f40612b1944debbf43aa3504
Adam Hill <adam@diginc.us>
2022-12-08 17:33:00 -0600

    Looked at some more old AOCs, this maybe a useful common + cleanup D8 more

--- aoc2022/day8.py
+++ aoc2022/day8.py
@@ -4,71 +4,63 @@ import pandas as pd
 from common import *
 from pprint import pprint
 
-
 # Setup
 DAY = 8
-filename = f"inputs/test{DAY}.txt"
-filename = f"inputs/day{DAY}.txt"
-grid = np.array([], dtype=int)
-x_size = 0
-input = read_input(filename)
-y_size = len(input)
-for line in input:
-    row = [int(t) for t in line]
-    x_size = len(row)
-    grid = np.append(grid, row)
 
-grid = np.reshape(grid, (x_size, y_size))
 
-p1_trees = 0
-p2_score = 0
-debug = [(3, 2)]
-for x in range(x_size):
-    for y in range(y_size):
-        # Part 1
-        loc = (x, y)
-        height = grid[loc]
-        # print(f"X: {x} Y: {y} is {height}")
-        # print(x, y, '=', height, grid[:x, y])  #, grid[x, :y], grid[x + 1:, y], grid[x, y + 1:])
-        up = grid[:x, y]
-        right = grid[x, y + 1:]
-        down = grid[x + 1:, y]
-        left = grid[x, :y]
-        visible = [
-            (left < height).all(),
-            (up < height).all(),
-            (right < height).all(),
-            (down < height).all()
-        ]
-        if np.any(visible):
-            p1_trees += 1
+def main(grid, answers=None):
+    ''' `grid` is data to solve today and optionally `answers` for refactor regression test '''
+    x_size, y_size = grid.shape
+    p1_trees = 0
+    p2_score = 0
+    debug = [(3, 2)]
+    for x in range(x_size):
+        for y in range(y_size):
+            # Part 1
+            loc = (x, y)
+            height = grid[loc]
+            if loc in debug:
+                print(f"X: {x} Y: {y} is {height}")
+            up = grid[:x, y]
+            right = grid[x, y + 1:]
+            down = grid[x + 1:, y]
+            left = grid[x, :y]
+            direction_visibility_checks = [
+                (left < height).all(),
+                (up < height).all(),
+                (right < height).all(),
+                (down < height).all()
+            ]
+            if np.any(direction_visibility_checks):
+                p1_trees += 1
 
-        # Part 2 could be coolers with slice iteration [::1] probably
-        score = 1
-        score_logger_debug = []
-        if loc in debug:
+            # Part 2
+            look_directions = [
+                np.flip(up), np.flip(left),  # Same as up[::-1], left[::-1],
+                right, down
+            ]
+            if loc in debug:
+                print(step_arrays, '\n', look_directions)
+            score = 1
             score_logger_debug = []
-        for look_dir in [
-            np.flip(up), np.flip(left),
-            right, down
-        ]:
-            dir_score = 0
-            for tree in look_dir:
-                dir_score += 1
-                if tree >= height:
-                    break
-            score_logger_debug.append(dir_score)
-            score *= dir_score
-        p2_score = max(score, p2_score)
-        # if y in [0, y_size] or x in [0, x_size]:
-        #     print(score_logger_debug, score)
+            for look_dir in step_arrays:
+                dir_score = 0
+                for tree in look_dir:
+                    dir_score += 1
+                    if tree >= height:
+                        break
+                score_logger_debug.append(dir_score)
+                score *= dir_score
+            p2_score = max(score, p2_score)
 
-print(f"Day {DAY} Part 1: {p1_trees}")
-print(f"Day {DAY} Part 2: {p2_score}")
+    print(f"Day {DAY} Part 1: {p1_trees}")
+    print(f"Day {DAY} Part 2: {p2_score}")
+    if answers:
+        assert p1_trees == answers[0]
+        assert p2_score == answers[1]
 
-if 'test' in filename:
-    assert p1_trees == 21
-    assert p2_score == 8
-else:
-    assert p1_trees == 1698
-    assert p2_score != 5762400
+
+test = get_chararray(f"inputs/test{DAY}.txt", dtype=int)
+main(test, answers=(21, 8))
+real = get_chararray(f"inputs/day{DAY}.txt", dtype=int)
+main(real, answers=(1698, 672280))
--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 import pathlib
 import sys
+import numpy as np
 
 
 def read_input(file, split='\n'):
@@ -11,3 +12,22 @@ def read_input(file, split='\n'):
     if split:
         return_me = return_me.strip().split(split)
     return return_me
+
+
+def get_chararray(filename, **kwargs):
+    '''
+    each character of the file will be returned as a numpy 2d array
+
+    Looked a little into char array under numpy but
+    they didn't work while I was rushing to solve night of
+    and this did work
+    '''
+    grid = np.array([], **kwargs)
+    input = read_input(filename)
+    x_size = -1
+    y_size = len(input)
+    for line in input:
+        row = [int(t) for t in line]
+        x_size = len(row)
+        grid = np.append(grid, row)
+    return np.reshape(grid, (x_size, y_size))
